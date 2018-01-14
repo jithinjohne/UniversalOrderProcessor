52f723411c19e0b1d443f8904488189281499e46
jithinjohne <jithinje@gmail.com>
2018-01-13 20:03:42 -0600

    Create translator

---
+++ UniversalOrderProcessor/IncomingTransaltor/XSLTranslator/XMLOrders.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace XSLTranslator
+{
+    public class XMLOrders : IFileList
+    {
+        private const string searchPattern = "*-cc.xml";
+        private const string path = @"C:\Source\Projects\UniversalOrderProcessor\UniversalOrderProcessor\Tests\SampleProcessing\IncomingTranslator";
+        private DirectoryInfo directoryInfo;
+
+        public XMLOrders()
+        {
+            directoryInfo = new DirectoryInfo(path);
+        }
+
+        public IEnumerable<FileInfo> GetOrders()
+        {
+            return directoryInfo.GetFiles(searchPattern);
+        }
+    }
+}
